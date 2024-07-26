e5a780b055295336e5f1ec5e653f55952addee2f
Saxon Payne <saxattack101@gmail.com>
2024-07-25 17:08:43 -0700

    create a file upload system so i can use the file to upload to facebook and other places

--- server/main.go
+++ server/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+
 	//"fmt"
 	"log"
 	"root/server/api/facebook"
@@ -11,6 +14,8 @@ import (
 var User *fb.User
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+
+	http.ServeFile(w, r, "../static/index.html")
 
 	if User != nil {
 		fmt.Println("I STORE THE VARIABLE HERE", User)
@@ -29,17 +34,41 @@ func AccessTokenHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func FaceBookImageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("hello server")
+func ImageHandler(w http.ResponseWriter, r *http.Request) {
+	file, handler, err := r.FormFile("fileToUpload")
+	if err != nil {
+		log.Println("error retrieving file", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	fmt.Println("1")
+	dst, err := os.Create(handler.Filename)
+	if err != nil {
+		log.Println("error creating file", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+	fmt.Println("1")
+
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "uploaded file")
 }
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static/"))))
+
+	fs := http.FileServer(http.Dir("../static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", Handler)
 	http.HandleFunc("/login", LoginHandler)
 	http.HandleFunc("/facebook_redirect", AccessTokenHandler)
 
-	http.HandleFunc("/facebook_upload_image", FaceBookImageHandler)
+	http.HandleFunc("/upload_image", ImageHandler)
 
 	fmt.Println("Starting server @ https://localhost:8080")
 	log.Fatal(http.ListenAndServeTLS(":8080", "../certificates/localhost.pem", "../certificates/localhost-key.pem", nil))
